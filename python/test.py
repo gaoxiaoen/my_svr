e948db2827e50a6863a81b00488180eff0ecccda
gaoxiaoen <648789030@qq.com>
2017-05-03 18:23:55 +0800

    add thread

---
+++ python/test.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+import socket
+print "create socket .."
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+print "done"
+print "connect to 192.168.87.129"
+s.connect(("192.168.87.129",7078))
+while True:
+	print "ye ,,"
+	sr = raw_input("send msg for 'q' is quit:")
+	if sr=='q':
+		break
+	else:
+		# sr = sr.decode("utf-8")
+		s.send(sr)
+	reply = s.recv(4096)
+	print reply
+
+	# sSend = raw_input()
+s.close()
+print "done"
---
+++ tThread.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void thread_1(int a)
+{
+	int i=0;
+	for(i=0;i<6;i++)
+	{
+		printf("this is a pthread_1..\n");
+		if(i==2)
+			pthread_exit(0);
+		sleep(1);
+	}
+}
+
+void thread_2(int a)
+{
+	int i;
+	for (int i = 0; i < 3; ++i)
+	{
+		printf("this is a pthread_2\n");
+	}
+	pthread_exit(0);
+}
+
+
+int main()
+{
+	pthread_t id_1,id_2;
+	int i,ret;
+	ret = pthread_create(&id_1,NULL,thread_1,6);
+	if (ret !=0)
+	{
+		printf("Create pthread error!\n");
+		return -1;
+	}
+
+	ret = pthread_create(&id_2,NULL,thread_2,7);
+	if (ret !=0)
+	{
+		printf("Create pthread error!\n");
+		return -1;
+	}
+	pthread_join(id_1,NULL);
+	pthread_join(id_2,NULL);
+
+	return 0;
+}
